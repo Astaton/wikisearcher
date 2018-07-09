24e7db56b0224aab1770b8e2af02727b56a646a4
Astaton <orson108@hotmail.com>
2018-07-09 18:19:30 -0400

    fixed bad variable

--- src/script.js
+++ src/script.js
@@ -32,43 +32,42 @@ $(document).ready(function(){
   }
     
   
-    function wikiSearch(search) {
-   var queryData = search;
+  function wikiSearch(search) {
+    var queryData = search;
   
-  $.ajax( {
-    url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search='+queryData+'&srlimit=25',
-    data: queryData,
-    dataType: 'json',
-    type: 'GET',
-    headers: { 'Api-User-Agent': 'WikiViewerProject/1.1 (https://codepen.io/Astaton/pen/MOomoV/; orson108@hotmail.com) BasedOnSuperLib/1.4' },
+    $.ajax( {
+      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search='+queryData+'&srlimit=25',
+      data: queryData,
+      dataType: 'json',
+      type: 'GET',
+      headers: { 'Api-User-Agent': 'WikiViewerProject/1.1 (https://codepen.io/Astaton/pen/MOomoV/; orson108@hotmail.com) BasedOnSuperLib/1.4' },
 
-    success: function(data) {
-      console.log(data)
-      var margin = parseInt($(".searchContainer").css("margin-top"));
-      if(margin >= 129){
-      shiftSearchBox();
-      }
-      if(data[1].length > 0 || data[2].length > 0 || data[3].length > 0){
-        renderResults(data);
-      }else{
-        renderNoResultsFound(data);
-      }
-      
-      var delay = 10;
-      //transition would not work with out fadeIn being delayed
-      fadeDelay = setInterval(function(){
-        if(delay <= 0){
-          fadeInResults();
-          clearInterval(fadeDelay);
+      success: function(data) {
+        console.log(data)
+        var margin = parseInt($(".searchContainer").css("margin-top"));
+        if(margin >= 129){
+        shiftSearchBox();
         }
-        delay--;
-      }, 20);
-    },
-    error: function(error){
-      console.log(error);
-    }
-
-   });
+        if(data[1].length > 0 || data[2].length > 0 || data[3].length > 0){
+          renderResults(data);
+        }else{
+          renderNoResultsFound(data);
+        }
+        
+        var delay = 10;
+        //transition would not work with out fadeIn being delayed
+        var fadeDelay = setInterval(function(){
+          if(delay <= 0){
+            fadeInResults();
+            clearInterval(fadeDelay);
+          }
+          delay--;
+        }, 20);
+      },
+      error: function(error){
+        console.log(error);
+      }
+    });
   };
 
   $('#searchButton').on('click', function(){
